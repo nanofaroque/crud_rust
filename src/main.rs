1ada4901a36452ea3182f4c0804d42f5eb30a310
ofaroque <ofaroque@amazon.com>
2020-06-06 13:07:40 -0500

    connection with local dynamodb instance

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, App, Responder, HttpServer, HttpResponse};
-
+use rusoto_core::Region;
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
 
 async fn index() -> impl Responder {
     "Hello world!"
@@ -20,6 +21,27 @@ async fn get_books() -> impl Responder{
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    let client = DynamoDbClient::new(Region::UsEast1);
+    let list_tables_input: ListTablesInput = Default::default();
+
+    match client.list_tables(list_tables_input).await {
+        Ok(output) => match output.table_names {
+            Some(table_name_list) => {
+                println!("Tables in database:");
+
+                for table_name in table_name_list {
+                    println!("{}", table_name);
+                }
+            }
+            None => println!("No tables in database!"),
+        },
+        Err(error) => {
+            println!("Error: {:?}", error);
+        }
+    }
+
+
+
     let mut v: Vec<String> = Vec::new();
     HttpServer::new(|| {
         App::new()
