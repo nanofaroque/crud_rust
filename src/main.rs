50970657a7d28eef0b17a1ea9fb42b7f96d2442d
ofaroque <ofaroque@amazon.com>
2020-06-06 14:35:57 -0500

    failed attempt to insert data into dynamodb

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
 use actix_web::{web, App, Responder, HttpServer, HttpResponse};
 use rusoto_core::Region;
-use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput, PutItemInput, AttributeValue, PutItemOutput, PutItemError};
+use std::collections::HashMap;
+
 
 async fn index() -> impl Responder {
     "Hello world!"
 }
 
-async fn add_book() ->impl Responder{
+async fn add_book() -> impl Responder {
+    let client = DynamoDbClient::new(Region::UsEast1);
+    let mut item: HashMap<String, AttributeValue> = HashMap::new();
+    item.insert("bookId".into(), AttributeValue {
+        s: Some(String::from("1")),
+        ..Default::default()
+    });
+    item.insert("name".into(), AttributeValue {
+        s: Some(String::from("Welcome to AWS")),
+        ..Default::default()
+    });
+    let put = PutItemInput {
+        table_name: "books".into(),
+        item,
+        ..Default::default()
+    };
+    client.put_item(put).await;
+
     let mut books: Vec<String> = Vec::new();
     books.push("new book".to_string());
     HttpResponse::Ok().body("A book has been added")
 }
 
-async fn get_books() -> impl Responder{
+async fn get_books() -> impl Responder {
     let mut books: Vec<String> = Vec::new();
     books.push("be rusty".to_string());
     books.push("be rustacean".to_string());
@@ -41,12 +60,11 @@ async fn main() -> std::io::Result<()> {
     }
 
 
-
     let mut v: Vec<String> = Vec::new();
     HttpServer::new(|| {
         App::new()
-            .route("/books",web::post().to(add_book))
-            .route("/books",web::get().to(get_books))
+            .route("/books", web::post().to(add_book))
+            .route("/books", web::get().to(get_books))
     })
         .bind("127.0.0.1:8088")?
         .run()
